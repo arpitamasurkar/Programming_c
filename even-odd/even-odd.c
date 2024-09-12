354038d64e8f895eed03a00d6fc3e06a6435740a
Arpita Sanjay Masurkar <arpitamasurkar28@gmail.com>
2024-09-12 17:36:14 +0530

    Update even-odd.c

--- even-odd/even-odd.c
+++ even-odd/even-odd.c
@@ -1,1 +1,14 @@
-hii
+#include<stdio.h>
+void main()
+{
+  int a;
+printf("enter the value of a");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("number is even");
+}
+else
+{
+  printf("number is odd");
+}
